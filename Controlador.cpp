15e8f766543aa6b9c85fba588734a206554d3214
Rorion4 <rorion.silva@ufv.br>
2022-12-12 10:45:20 -0300

    Mecânica de mapas e Controlador

--- Controlador.cpp
+++ Controlador.cpp
@@ -1,16 +1,16 @@
 #include "Controlador.h"
 
-Controlador::Controlador() {
+//Controlador::Controlador() {
     
    
-}
+//}
    
 
-Controlador::~Controlador(){}
+//Controlador::~Controlador(){}
 
-char Controlador::escolherMapa() {
+//char Controlador::escolherMapa() {
    // if(vidaInimigo<=0)
    //     return _cont++;
    // else
     //    return 0; 
-}
+//}
--- README.md
+++
@@ -1,4 +0,0 @@
-# 20222-team-10
-20222-team-10
-
-Projeto de um jogo estilo RPG focado em combate em fases.
